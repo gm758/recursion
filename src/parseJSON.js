d11d7209b27af00a501a3be8432b242bb47c256c
Griffin Michl <griffinmichl@gmail.com>
2015-12-02 14:21:47 +0700

    beginning work on parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,18 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  if (json === "null") return null;
+  if (json === "true") return true;
+  if (json === "false") return false;
+  if (!isNaN(json)) return +json
+  if (json[0] === "[") 
+	return json.slice(1,-1).split(",")
+  
+  if (json[0] === "{") {
+	var jsonObj = {};
+	
+  }
+  
+  
+  
 };
